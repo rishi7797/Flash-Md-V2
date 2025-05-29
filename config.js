f6b5537fb2ac8022a7e0b95b2b9a7fe9abe07838
rishi7797 <godgaming8000@gmail.com>
2025-05-30 00:19:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const conf = {
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "on",
   AUTO_LIKE: process.env.AUTO_LIKE || "on",
   AUTO_READ_DM: process.env.AUTO_READ_DM || "off",
-  AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || "off",
+  AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || "on",
   ADM: process.env.ANTI_DELETE || "on",
   ON: process.env.OWNER_NAME || "FLASH-MD",
   ANTICALL: process.env.ANTICALL || "off",
@@ -22,7 +22,7 @@ const conf = {
   ? process.env.PREFIX.split(",").map(p => p.trim()) 
   : [""], 
   Session: process.env.SESSION || "",
-  NUMBER: process.env.OWNER_NUMBER || "",
+  NUMBER: process.env.OWNER_NUMBER || "917478191921",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || ""
 };
